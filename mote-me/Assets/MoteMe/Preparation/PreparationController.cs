bc79ca1ed9e834b7cae10db517590a6dfcf51c9c
faidra <faidra.windmark@gmail.com>
2019-02-17 18:38:10 +0900

    シーンチェンジできるようにした

---
+++ mote-me/Assets/MoteMe/Preparation/PreparationController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UniRx;
+using UniRx.Async;
+using UnityEngine.UI;
+
+public class PreparationController : MonoBehaviour
+{
+    [SerializeField]
+    Button PlayButton;
+
+    void Start()
+    {
+        PlayButton.OnClickAsObservable()
+            .Subscribe(_ => GoToPlay())
+            .AddTo(this);
+    }
+
+    void GoToPlay()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("test", UnityEngine.SceneManagement.LoadSceneMode.Single).ToUniTask().Forget();
+    }
+}
