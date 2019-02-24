5e474ce7692fc8fa69b28735b5491e49654589fa
faidra <faidra.windmark@gmail.com>
2019-02-24 20:16:07 +0900

    Resultシーンを雑に作成

---
+++ mote-me/Assets/MoteMe/Result/ResultController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UniRx;
+using UniRx.Async;
+using UnityEngine.UI;
+using Zenject;
+
+public class ResultController : MonoBehaviour
+{
+    [SerializeField]
+    Button RetryButton;
+    [SerializeField]
+    Button WatchButton;
+    [SerializeField]
+    Button BackButton;
+
+    [Inject]
+    SceneManager SceneManager;
+
+    void Start()
+    {
+        Observable.Merge(
+            RetryButton.OnClickAsObservable().Select(_ => "test"),
+            WatchButton.OnClickAsObservable().Select(_ => "watch"),
+            BackButton.OnClickAsObservable().Select(_ => "preparation"))
+        .First()
+        .Select(SceneManager.ChangeToAsync)
+        .Subscribe(task => task.Forget())
+        .AddTo(this);
+    }
+}
